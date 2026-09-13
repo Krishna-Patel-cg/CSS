047728a22b2a327241cbaa934d638f64b0ffad7f
Krishna-Patel-cg <krishna.p.patel.cg@gmail.com>
2026-09-13 23:52:08 +0530

    assignment added

---
+++ assignment/class05assigment.js
@@ -0,0 +1,12 @@
+function blackBg(){
+    document.body.style.backgroundColor ="black"
+}
+function blueBg(){
+    document.body.style.backgroundColor ="blue"
+}
+function greenBg(){
+    document.body.style.backgroundColor ="green"
+}
+function redBg(){
+    document.body.style.backgroundColor ="red"
+}
